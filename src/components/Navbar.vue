51c22d839ac19b55e3b4f76ade5179cc6cc69ab2
ABow_Chen <fred39392001@gmail.com>
2020-08-21 01:07:50 +0800

    update banner img

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="bg-primary sticky-top">
       <div class="container">
-        <nav class="navbar px-0 py-3 navbar-expand-lg navbar-light bg-primary">
+        <!-- <nav class="navbar px-0 py-3 navbar-expand-lg navbar-light bg-primary">
           <a class="navbar-brand position-absolute text-gold" href="./index.html"
             style="left: 50%; transform: translate(-50%, -50%); top: 50%;">
             <img src="../../public/img/Fitness_Kitchen_Logo.svg"  style="height:55px" alt="">
@@ -26,6 +26,33 @@
           <div class="d-flex">
             <a class="text-gold" href="./cart-2.html">
             <i class="fas fa-shopping-cart"></i></a>
+          </div>
+        </nav> -->
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+          <a class="navbar-brand" href="#">
+            <img src="../../public/img/Fitness_Kitchen_Logo.svg"  style="height:55px" alt="">
+          </a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse"
+          data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+          aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1"
+                aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
           </div>
         </nav>
       </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,26 @@
 <template>
   <div>
     <div class="container">
-      <!-- <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
           <div class="carousel-item active">
-              <img src="..." class="d-block w-100" alt="">
+            <img src="../../public/img/banner-01.jpg" class="d-block w-100" alt="...">
           </div>
           <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="..." class="d-block w-100" alt="...">
+            <img src="../../public/img/banner-02.jpg" class="d-block w-100" alt="...">
           </div>
         </div>
-      </div> -->
+        <a class="carousel-control-prev" href="#carouselExampleControls"
+        role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls"
+        role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
       <div class="row mt-5">
         <div class="col-md-6 mt-md-4">
           <div class="card border-0 mb-4 position-relative position-relative">
